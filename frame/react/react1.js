7584a348056523969388be3db8d976fcd1e86d04
zhangzs <zhangzesheng@gaosiedu.com>
2018-04-16 11:06:01 +0800

    add event about react

--- frame/react/react1.js
+++ frame/react/react1.js
@@ -1,9 +1,22 @@
 class LikeButton {
+
+	constructor () {
+
+	}
+
 	render() {
-		return`
+		this.ele = string2dom(`
 			 <button class='like-btn'>
 		        <span class='like-text'>点赞</span>
 		      </button>
-			`
+			`)
+		this.ele.addEventListener('click',()=>console.log(111))
+		return this.ele;
 	}
+}
+function string2dom(str) {
+	let wrapper = document.createElement('div');
+	wrapper.class="wrapper";
+	wrapper.innerHTML = str;
+	return wrapper;
 }

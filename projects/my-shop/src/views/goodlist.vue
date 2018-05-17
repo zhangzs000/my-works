06a0f11beba4beb37f3486352be4219a6fa5952d
zhangzs000 <1461998312@qq.com>
2018-05-17 08:21:12 +0800

    test

---
+++ projects/my-shop/src/views/goodlist.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <div>
+            <ul>
+                <li v-for="(item, index) in goodList">
+                    
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+ import axios from 'axios'
+    export default{
+        data() {
+            return {
+                
+        },
+        mounted(){
+            // this.getGoodsList();
+        },
+        components: {
+           
+        },
+        methods: {
+            getGoodsList() {
+                axios.get('/goods').then((result)=>{
+                    console.log(result)
+                })
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>

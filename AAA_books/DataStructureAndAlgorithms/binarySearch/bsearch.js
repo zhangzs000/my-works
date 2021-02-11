24376f92ff861e1a68add945a0fa8628dacc7cf5
zhangzs000 <1461998312@qq.com>
2021-02-11 09:52:56 +0800

    Update bsearch.js

--- AAA_books/DataStructureAndAlgorithms/binarySearch/bsearch.js
+++ AAA_books/DataStructureAndAlgorithms/binarySearch/bsearch.js
@@ -19,3 +19,24 @@ const A = [3,5,19,22,25,33,45,47,57 ,66, 71,78]
 console.log( bsearch(A, 88) )
 console.log( bsearch(A, 68) )
 console.log( bsearch(A, 22 ) )
+
+
+// l = m+1, r = m-1, 判断条件 l<=m, 中间 val>nums[m], > 和 >= 不影响结果
+const binarySearch = ()=>{
+	let nums = [1,2,3,4,5,6];
+	let val = 4;
+	let r = nums.length;
+    let l = 0;
+    while(l<=r){
+        let m = Math.floor((l+r)/2);
+        if(val>nums[m]){
+            l = m+1;
+        }else {
+            r = m-1;
+        }
+    }
+    // val>=nums[m]  l==4,稳定;  val>nums[m] l==3 不稳定
+    nums.splice(l, 0, val);
+    return nums
+}
+console.log('xxx: ',xxx())
